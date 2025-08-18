5a46e4150cadf8527064902e0f18cca08d99c46e
{Harshit-Learn} <harshitbaranwal609@gmail.com>
2025-08-18 14:20:06 +0530

    Node

---
+++ Day_3_npm_init/index.js
@@ -0,0 +1,7 @@
+// npminit : inilisation the npm
+// Install the  npm nodemon for automatic start server : npm i node
+ : 
+console.log("hello node js")
+
+console.log( 45*4)
+console.log("hello node js")
