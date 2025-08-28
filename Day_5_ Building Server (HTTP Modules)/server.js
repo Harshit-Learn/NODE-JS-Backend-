807f0823ab6e91b5305a43b91b95920a5416559f
{Harshit-Learn} <harshitbaranwal609@gmail.com>
2025-08-28 14:56:28 +0530

    Http Module

--- Day_5_ Building Server (HTTP Modules)/server.js
+++ Day_5_ Building Server (HTTP Modules)/server.js
@@ -4,5 +4,6 @@ const server = http.createServer((req , res)=>{
     res.end("You  requested for somthing!!")     // This is response from your server . Link: http://localhost:3000/
 });
 
+ // Server Start
 const port = 2000;
 server.listen(port, ()=> console.log(`Server is running on port ${port}`))
