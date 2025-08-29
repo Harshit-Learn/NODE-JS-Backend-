914e3353e68610e9952d71add87a127562ec078e
{Harshit-Learn} <harshitbaranwal609@gmail.com>
2025-08-30 00:04:42 +0530

    Set Static file in EJS

---
+++ Day_10_EJS_SSR/server.js
@@ -0,0 +1,28 @@
+//  Template Engine (ejs) || SSR ( Server Side Rendering )
+//  EJS = tool for embedding JS into HTML templates.
+// SSR = technique of rendering the final HTML on the server before sending it to the browser.
+
+// npm init -y
+//npm i express 
+// npm i ejs   install EJS 
+
+// Note: EJS file send krne ke liye res.render() use krte he similarly For HTML - res.send() , HTML file - res.sendFile()  & Foe Json- res.json()
+
+// HTML ko view folder ke under hi create krte he
+
+import express from 'express'
+
+// Arrar
+let products = [
+    {title: "RealMe" , price:32000},
+    {title: "Moto" , price:33000},
+    {title: "JIO" , price:42000}
+]
+const app = express();
+app.get('/' , (req , res)=>{            // Home route (/)
+let name = 'harshit'             
+res.render('index.ejs' , {name , products} )      // For EJS - res.render()  & Add dynamic value in index.html
+})
+
+const port = 3000;
+app.listen(port , ()=>console.log(`Server is running on port ${port}`))
