361bf1d0245930e818a6973919bb30ed42eade9a
{Harshit-Learn} <harshitbaranwal609@gmail.com>
2025-09-02 10:50:17 +0530

    Form Handling coments add

--- Day_12_ Form Handling/server.js
+++ Day_12_ Form Handling/server.js
@@ -2,6 +2,18 @@
 //npm init -y
 // npm i express ejs
 
+// How it Works
+// User form me data fill karta hai → method="POST"  .,ke saath backend ko bhejta hai.
+
+
+// express.urlencoded() middleware form data ko parse karke req.body me store karta hai.
+
+
+// Backend req.body se data nikal ke process karta hai (DB save, validation, etc.).
+
+
+
+
 import express from 'express'
 
 const app = express();
